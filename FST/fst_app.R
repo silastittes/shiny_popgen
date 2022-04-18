7688dda26d3d094525ec0067e7831104393c656e
silastittes <silastittes@gmail.com>
2022-04-18 14:55:19 -0600

    representing each individual as a a segmented rectangle with two colors

--- FST/fst_app.R
+++ FST/fst_app.R
@@ -9,6 +9,23 @@
 
 library(tidyverse)
 
+duel_rect <- function(x, y, size_x, size_y, col1 = NA, col2 = NA, ...){
+  
+  rect(xleft = x - size_x, 
+       ybottom = y - size_y, 
+       xright = x, 
+       ytop = y + size_y, col = col1, ...
+  )  
+  rect(xleft = x, 
+       ybottom = y - size_y, 
+       xright = x + size_x, 
+       ytop = y + size_y, col = col2, ...
+  )  
+}
+
+#duel_rect(x = 0, 0, .1, .1, col1 = "red", col2 = "blue")
+
+
 het <- function(x){
   2 * mean(x == 1) * mean(x == 2)
 }
@@ -74,7 +91,7 @@ server <- function(input, output){
     coords <- data.frame(c(ell_x, rev(ell_x)),
                          c(ell_y, -ell_y))
     
-    spp <- 1.3
+    spp <- 3
     plot(coords[[1]], coords[[2]], type ="n", asp = 0.8, axes = F, 
          xlab = "", ylab = "")
     polygon(coords[[1]] - spp, coords[[2]], col = ell_col, lwd =2)
@@ -89,7 +106,7 @@ server <- function(input, output){
     
     adj1 <- 0.1
     sz <- 3
-    shpe <- c(21, 21)
+    shpe <- c(22, 22)
     
     colr <- c(rgb(1, 0, 0, .9),
               rgb(0, 0, 1, .9))
@@ -102,15 +119,20 @@ server <- function(input, output){
     loc1 <- sample_n(coords, data_out$N)*locations
     loc2 <- sample_n(coords, data_out$N)*locations
     
-    points(loc1[[1]] - spp, loc1[[2]], pch = shpe[alleles_d1[,1]], 
-           bg = colr[alleles_d1[,1]], cex = sz)
-    points((loc1[[1]] - spp) - adj1, loc1[[2]], pch = shpe[alleles_d1[,2]], 
-           bg = colr[alleles_d1[,2]], cex = sz)
+    #points(loc1[[1]] - spp, loc1[[2]], pch = shpe[alleles_d1[,1]], 
+    #       bg = colr[alleles_d1[,1]], cex = sz)
     
-    points((loc2[[1]] + spp) - adj1, loc2[[2]], pch = shpe[alleles_d2[,2]], 
-           bg = colr[alleles_d2[,2]], cex = sz)
-    points(loc2[[1]] + spp, loc2[[2]], pch = shpe[alleles_d2[,1]], 
-           bg = colr[alleles_d2[,1]], cex = sz)
+    #points((loc1[[1]] - spp) - adj1, loc1[[2]], pch = shpe[alleles_d1[,2]], 
+    #       bg = colr[alleles_d1[,2]], cex = sz)
+    
+    #points((loc2[[1]] + spp) - adj1, loc2[[2]], pch = shpe[alleles_d2[,2]], 
+    #       bg = colr[alleles_d2[,2]], cex = sz)
+    
+    #points(loc2[[1]] + spp, loc2[[2]], pch = shpe[alleles_d2[,1]], 
+    #       bg = colr[alleles_d2[,1]], cex = sz)
+    sz <- .1
+    duel_rect(loc1[[1]] - spp, loc1[[2]], col1 = colr[alleles_d1[,1]], col2 = colr[alleles_d1[,2]], size_x = sz, size_y = sz)
+    duel_rect(loc2[[1]] + spp, loc2[[2]], col1 = colr[alleles_d2[,1]], col2 = colr[alleles_d2[,2]], size_x = sz, size_y = sz)
     
     hs <- round(mean(c(het(alleles_d1), het(alleles_d2))),3)
     ht <- round(het(c(alleles_d1, alleles_d2)),3)
@@ -121,7 +143,7 @@ server <- function(input, output){
       #                            F[ST] = .(fst)),
        #    merge=TRUE, bty = "n")
     
-    legend(-4.5, -0.5,
+    legend(-5.6, -0.5,
            c(eval(substitute( expression(H[S] == hs))),
              eval(substitute( expression(H[T] == ht))),
              eval(substitute( expression(F[ST] == fst)))
