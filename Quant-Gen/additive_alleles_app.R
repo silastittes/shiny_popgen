9a401452a9cd84c5c1451a57916079f350eadf81
Silas Tittes <silas.tittes@gmail.com>
2022-10-05 14:04:23 -0600

    added environmental variance

--- Quant-Gen/additive_alleles_app.R
+++ Quant-Gen/additive_alleles_app.R
@@ -1,35 +1,40 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application You can run the application by clicking
+# the 'Run App' button above
 #
 # Find out more about building applications with Shiny here:
 #
-#    http://shiny.rstudio.com/
+#    http://shiny_rstudio_com/
 #
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+theme_set(theme_classic(15))
 
-pheno.dist <- function(n.loci, allele.types) {
-  n.alleles <- 2 * n.loci #2 alleles for each locus
-  class.counts <- choose(n.alleles,
-                         0:n.alleles)
+pheno_dist <- function(n_loci, allele_types, v_env) {
+  n_alleles <- 2 * n_loci #2 alleles for each locus
+  class_counts <- choose(n_alleles,
+                         0:n_alleles)
   # turn counts into proportions
-  class.freqs <- class.counts/sum(class.counts)
-  #class.freqs <- class.counts
-  class.types <- NULL #storage
-  for (i in 0:n.alleles) {
-    
-    type <- i * allele.types[1] +
-      (n.alleles - i) * allele.types[2]
-    class.types <- c(class.types, 
-                     type) #add to storage
-  }
-  return(list(class.types = class.types,
-              class.freqs = class.freqs))
+  class_freqs <- class_counts/sum(class_counts)
   
+  class_types <- (0:n_alleles) * allele_types[1] + 
+                 (n_alleles - (0:n_alleles)) * allele_types[2]
+  
+  additive_phenotype <- rep(class_types, class_counts) 
+  phenotype <- rnorm(n=length(additive_phenotype), mean = additive_phenotype, sd = v_env)
+  
+  return(
+    tibble(
+      additive_phenotype = additive_phenotype,
+      phenotype = phenotype
+    )
+  )
 }
 
-#for_sim(n = 10, gen = 20, show_coal = F)
+#pheno_dist(n_loci = 10, allele_types = c(0, 10), 2)$phenotype
 
 ui <- fluidPage(pageWithSidebar( 
   
@@ -38,14 +43,14 @@ ui <- fluidPage(pageWithSidebar(
   sidebarPanel(
     
     sliderInput(inputId = "n", label = "Number of independent loci", value = 1, 
-                min = 1, max = 100, step = 1),
+                min = 0, max = 15, step = 1),
     sliderInput(inputId = "A", label = "Phenotypic contribution of A alleles", value = 10, 
-                min = 0, max = 100, step = 1),
+                min = 0, max = 10, step = 1),
     sliderInput(inputId = "a", label = "Phenotypic contribution of a alleles", value = 0, 
-                min = 0, max = 100, step = 1)
-
+                min = 0, max = 10, step = 1),
+    sliderInput(inputId = "e", label = "Environmental contribution to phenotype variance", value = 0, 
+                min = 0, max = 20, step = .5)
   ), 
-  
   mainPanel =  mainPanel(
     plotOutput(outputId = 'viz')
   )
@@ -56,13 +61,20 @@ ui <- fluidPage(pageWithSidebar(
 server <- function(input, output){
   
   output$viz <- renderPlot({
-    out <- pheno.dist(input$n, c(input$A, input$a))
-    plot(out$class.types, out$class.freqs,
-         type = "h", lwd = 3, ylab = "Frequency of phenotypic class",
-         xlab = "Phenotypic classes", ylim = c(0, max(out$class.freqs, na.rm = F)*1.05))
-    
+    out <- pheno_dist(input$n, c(input$A, input$a), input$e)
+    p1 <- ggplot(data = out) +
+      geom_histogram(aes(x=additive_phenotype), binwidth = 1) +
+      ylab("Frequency") +
+      xlab("Additive Phenotype")
+    p2 <- ggplot(data = out) +
+      geom_histogram(aes(x=phenotype), binwidth = 1) +
+      ylab("Frequency") +
+      xlab("Phenotype")
+    p1 + p2 + plot_layout(ncol = 1)
   })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
